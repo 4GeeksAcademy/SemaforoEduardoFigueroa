c53931edc743db48b9a4447766fc6cd003d08f71
Eduardo Figueroa <156800901+EduardoFigueroa92@users.noreply.github.com>
2024-04-25 07:01:29 +0000

    error solucionado, funciona bien

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [selectedColor, setSelectedColor] = useState("");
+	const [colors, setColors] = useState(["red", "yellow", "green"]);
+	const [intervalId, setIntervalId] = useState(null);
+	const [purple, setPurple] = useState(false);
+
+	useEffect(() => {
+		if (intervalId !== null) {
+			clearInterval(intervalId);
+			const id = setInterval(changeColor, 1000);
+			setIntervalId(id);
+		}
+	}, [colors]);
+
+	const changeColor = () => {
+		setSelectedColor(currentColor => {
+			const currentIndex = colors.indexOf(currentColor);
+			if (currentColor === colors.length - 1) {
+				return purple ? "purple" : colors[0];
+			} else {
+				return colors[currentIndex + 1];
+			}
+		});
+	};
+
+	const toggleInterval = () => {
+		if (intervalId) {
+			clearIntervalId(intervalId);
+			setIntervalId(null);
+		} else {
+			const id = setInterval(changeColor, 1000);
+			setIntervalId(id);
+		}
+	};
+
+	const togglePurple = () => {
+		setPurple(!purple);
+		if (!purple) {
+			setColors(prevColors => [...prevColors, "purple"]);
+		} else {
+			setColors(prevColors => prevColors.filter(color => color !== "purple"));
+		}
+	};
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+		<div className="fondo">
+			<div className="text-center mt-5">
+				<div className={purple ? "contenedorSemaforoPurple" : "contenedorSemaforo"}>
+					{colors.map(color => (
+						<div
+							key={color}
+							onClick={() => setSelectedColor(color)}
+							className={`light${color.charAt(0).toUpperCase() + color.slice(1)}` + (selectedColor === color ? " glow" : "")}
+						/>
+					))}
+				</div>
+				<button onClick={togglePurple}>{purple ? "Quitar color" : "Agregar color"}</button>
+				<button onClick={toggleInterval}>Encender semáforo</button>
+			</div>
 		</div>
 	);
 };
